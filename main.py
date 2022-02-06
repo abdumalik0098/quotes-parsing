38446241abef700562c69a697b5847f185109081
malik <mister.ak98@list.ru>
2022-02-06 16:47:39 +0700

    task parsing in py

---
+++ main.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+
+
+headers = {
+    "accept": "*/*",
+    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Mobile Safari/537.36"
+}
+
+
+for i in range(1, 11):
+    url = f"https://quotes.toscrape.com/page/{i}/"
+    req = requests.get(url, headers=headers)
+    src = req.text
+
+    soup = BeautifulSoup(src, 'lxml')
+    quotes = soup.find_all(class_="quote")
+
+    quotes_list = []
+
+    for item in quotes:
+        text = item.find(class_="text").text
+        author = item.find(class_="author").text
+        author_link = "https://quotes.toscrape.com" + item.find("a").get("href")
+        tags = item.find(class_="keywords").get("content")
+        
+        quotes_list.append(
+            {
+                "Text": text,
+                "Author": author,
+                "About author": author_link,
+                "Tags": tags
+            }
+        )
+
+    with open(f"list_{i}.json", "a", encoding="utf-8") as file:
+        json.dump(quotes_list, file, indent=4, ensure_ascii=False)
